a8d7a0128d378e4c2fd070f07c48f2528e9535ca
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-08-13 21:46:11 +0300

    setup project skeleton

---
+++ script.js
@@ -0,0 +1,14 @@
+//constructor for making book object
+
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${isRead}`;
+    }
+}
+
+
+// console.log(theHobbit.info());
