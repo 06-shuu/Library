5a1ae58ae1ef5daa03b373a2c2bff4ae6f582774
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-08-14 22:10:28 +0300

    render books array in the main htl file

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-//constructor for making book object
 
-function Book(title, author, pages, isRead){
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = function(){
-        return `${title} by ${author}, ${pages} pages, ${isRead}`;
-    }
 }
 
+let book1 = new Book('Kafka on the shore', 'Haruki Murakami', 480, 'yes');
+let book2 = new Book('Emily the Strange', 'Jessica Gruner', 264, 'no')
+let book3 = new Book('Siyar Al-muluk', 'Nizam Al-mulk', 325, 'no')
 
-// console.log(theHobbit.info());
+function addBookToLibrary() {
+    myLibrary.push(book1)
+    myLibrary.push(book2)
+    myLibrary.push(book3)
+}
+
+addBookToLibrary() //call the function too add books into array 
+
+Book.prototype.display = function () {
+    //here the book will be displayed in a card 
+    return `
+    <p>Title: ${this.title}</p>
+    <p>Author: ${this.author}</p>
+    <p>Pages: ${this.pages}</p>
+    <p>Read: ${this.isRead}</p>
+    `
+}
+
+function displayBooks() {
+    const showBooks = document.querySelector(".showBooks")
+    
+    myLibrary.forEach((book, index) => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book-card');
+        bookDiv.innerHTML = `<div> ${book.display()} </div>`;
+        showBooks.appendChild(bookDiv);
+    });
+}
+
+displayBooks()
