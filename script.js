8f5c0204ffbd2a0efcbb5759f0e8fc41ac2834e6
Shumokh <154021913+06-shuu@users.noreply.github.com>
2024-08-15 05:53:39 +0300

    style the whole page and add fontawesome and render the dialong

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, rate) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.rate = rate;
 }
 
-let book1 = new Book('Kafka on the shore', 'Haruki Murakami', 480, 'yes');
-let book2 = new Book('Emily the Strange', 'Jessica Gruner', 264, 'no')
-let book3 = new Book('Siyar Al-muluk', 'Nizam Al-mulk', 325, 'no')
+let book1 = new Book('Kafka on the shore', 'Haruki Murakami', 480, 'yes','☆☆☆☆');
+let book2 = new Book('Emily the Strange', 'Jessica Gruner', 264, 'no','☆☆☆')
+let book3 = new Book('Siyar Al-muluk', 'Nizam Al-mulk', 325, 'no','☆☆☆')
+
 
 function addBookToLibrary() {
     myLibrary.push(book1)
@@ -18,27 +20,49 @@ function addBookToLibrary() {
     myLibrary.push(book3)
 }
 
-addBookToLibrary() //call the function too add books into array 
+addBookToLibrary() 
 
 Book.prototype.display = function () {
-    //here the book will be displayed in a card 
     return `
-    <p>Title: ${this.title}</p>
+    <p class="title">${this.title}</p>
     <p>Author: ${this.author}</p>
     <p>Pages: ${this.pages}</p>
     <p>Read: ${this.isRead}</p>
+    <p>Rate: ${this.rate}</p>
     `
 }
+
+//
+const showButton = document.querySelector(".showDialog");
+const addBookDialog = document.querySelector(".addBookDialog");
+const outputBox = document.querySelector("output");
+const selectReadingStatus = addBookDialog.querySelector('#isRead')
+const selectRating = addBookDialog.querySelector('#rate')
+const confirmBtn = addBookDialog.querySelector("#confirmBtn");
+
+showButton.addEventListener("click", () => {
+    addBookDialog.showModal();
+});
+
 
 function displayBooks() {
     const showBooks = document.querySelector(".showBooks")
     
-    myLibrary.forEach((book, index) => {
+    myLibrary.forEach((book) => {
         const bookDiv = document.createElement('div');
         bookDiv.classList.add('book-card');
-        bookDiv.innerHTML = `<div> ${book.display()} </div>`;
+        bookDiv.innerHTML = `
+            ${book.display()} 
+            <div class="ebtns">
+                <button class="deleteBtn"><i class="fa-solid fa-trash-can"></i></button>
+                <button class=editBtn><i class="fa-solid fa-pen-to-square"></i></button>
+            </div>
+            `;
         showBooks.appendChild(bookDiv);
     });
 }
 
 displayBooks()
+
+
+
